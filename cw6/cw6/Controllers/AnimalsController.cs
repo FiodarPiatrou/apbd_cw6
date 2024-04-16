de3911b5ee49cc16903c9e39646c1452e27dfbcc
s22854 <s22854@pjwstk.edu.pl>
2024-04-16 13:36:25 +0200

    Post and Get

---
+++ cw6/cw6/Controllers/AnimalsController.cs
@@ -0,0 +1,60 @@
+using cw6.DTOs;
+using cw6.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace cw6.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        using SqlConnection connection= new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM Animal";
+        var reader = command.ExecuteReader();
+        
+        List<Animal> animals = new List<Animal>();
+        int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int nameOrdinal = reader.GetOrdinal("Name");
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                IdAnimal = reader.GetInt32(idAnimalOrdinal),
+                Name = reader.GetString(nameOrdinal),
+                
+            });
+            
+               
+        }
+
+        return Ok(animals);
+    }
+
+    [HttpPost]
+    public IActionResult AddAnimal(AddAnimal animal)
+    {
+        using SqlConnection connection= new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "INSERT INTO Animal VALUES (@animalName,@animalCategory,' ',' ')";
+        command.Parameters.AddWithValue("@animalName", animal.Name);
+        command.Parameters.AddWithValue("@animalCategory", animal.Category);
+        command.ExecuteNonQuery();
+        return Created("", null);
+    }
+    
+
+}
