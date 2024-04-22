570fcda385dac3ee100ca8bfdff2283b3756f123
Fiodar Piatrou <s22854@pjwstk.edu.pl>
2024-04-22 13:54:02 +0200

    Finished PUT And POST

--- cw6/cw6/Controllers/AnimalsController.cs
+++ cw6/cw6/Controllers/AnimalsController.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using cw6.Models;
 using cw6.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
@@ -10,33 +11,40 @@ namespace cw6.Controllers;
 public class AnimalsController : ControllerBase
 {
     private readonly IConfiguration _configuration;
+
     public AnimalsController(IConfiguration configuration)
     {
         _configuration = configuration;
     }
+
     [HttpGet]
     public IActionResult GetAnimals()
     {
-        using SqlConnection connection= new SqlConnection(_configuration.GetConnectionString("Default"));
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
         connection.Open();
         using SqlCommand command = new SqlCommand();
         command.Connection = connection;
         command.CommandText = "SELECT * FROM Animal";
         var reader = command.ExecuteReader();
-        
+
         List<Animal> animals = new List<Animal>();
         int idAnimalOrdinal = reader.GetOrdinal("IdAnimal");
         int nameOrdinal = reader.GetOrdinal("Name");
+        int descriptionOrdinal = reader.GetOrdinal("Description");
+        int categoryOrdinal = reader.GetOrdinal("Category");
+        int areaOrdinal = reader.GetOrdinal("Area");
+
+        
         while (reader.Read())
         {
             animals.Add(new Animal()
             {
                 IdAnimal = reader.GetInt32(idAnimalOrdinal),
-                Name = reader.GetString(nameOrdinal),
-                
+                Name = reader.GetString(nameOrdinal), 
+                //Description = reader.GetString(descriptionOrdinal),
+                Category = reader.GetString(categoryOrdinal),
+                Area = reader.GetString(areaOrdinal)
             });
-            
-               
         }
 
         return Ok(animals);
@@ -45,16 +53,37 @@ public class AnimalsController : ControllerBase
     [HttpPost]
     public IActionResult AddAnimal(AddAnimal animal)
     {
-        using SqlConnection connection= new SqlConnection(_configuration.GetConnectionString("Default"));
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
         connection.Open();
         using SqlCommand command = new SqlCommand();
         command.Connection = connection;
-        command.CommandText = "INSERT INTO Animal VALUES (@animalName,@animalCategory,' ',' ')";
+        command.CommandText = "INSERT INTO Animal VALUES (@animalName,@animalCategory,@animalDescription ,@animalArea)";
         command.Parameters.AddWithValue("@animalName", animal.Name);
         command.Parameters.AddWithValue("@animalCategory", animal.Category);
+        command.Parameters.AddWithValue("@animalDescription", animal.Description);
+        command.Parameters.AddWithValue("@animalArea", animal.Area);
         command.ExecuteNonQuery();
         return Created("", null);
     }
-    
+    [HttpPut("{idAnimal}")]
+    public IActionResult UpdateAnimal(int idAnimal,[FromBody]AddAnimal addAnimal)
+    {
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "UPDATE Animal SET Name=@animalName," +
+                              "Description=@animalDescription," +
+                              "Category=@animalCategory," +
+                              "Area=@animalArea WHERE IdAnimal=@idAnimal" ;
+        command.Parameters.AddWithValue("@idAnimal", idAnimal);
+        command.Parameters.AddWithValue("@animalName", addAnimal.Name);
+        command.Parameters.AddWithValue("@animalCategory", addAnimal.Category);
+        command.Parameters.AddWithValue("@animalDescription", addAnimal.Description);
+        command.Parameters.AddWithValue("@animalArea", addAnimal.Area);
+        command.ExecuteNonQuery();
+        return Ok();
+        
 
+    }
 }
